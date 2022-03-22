d3999b81b230430768b1b3ba1e28d591657354ce
Bart Koelewijn <bartkoelewijn@bluenotion.nl>
2022-03-22 19:47:59 +0100

    set base path.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   images: {
     loader: 'akamai',
     path: '',
