f2d6b3767dde364eb5e70353aaf86fa39cf50317
Bart0810 <bart_koelewijn@hotmail.nl>
2022-03-23 11:40:30 +0000

    Deploying to gh-pages from main @ b17eeea93cb139a81ae9c35a6efb4b1c6f766616 🚀

---
+++ _next/static/2K-EFtQk6je6bod8XtZzN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/528-4a2620c5cda4683b.js","static/chunks/pages/index-59edcf78fc165cc4.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
