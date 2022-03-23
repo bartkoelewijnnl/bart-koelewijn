e1b68ea03433a3dc582034116c2d7416404ae4c2
Bart Koelewijn <bartkoelewijn@bluenotion.nl>
2022-03-23 09:27:23 +0100

    Lighthouse

--- components/IntroCta/index.tsx
+++ components/IntroCta/index.tsx
@@ -9,7 +9,7 @@ const IntroCta: FC<IntroCtaProps> = () => {
     return (
         <Cta>
             <Dots isSmall />
-            <Link to="awaretrain" smooth offset={-64}>
+            <Link aria-label="scroll naar inhoud" to="awaretrain" smooth offset={-64}>
                 <RoundButton />
             </Link>
         </Cta>
