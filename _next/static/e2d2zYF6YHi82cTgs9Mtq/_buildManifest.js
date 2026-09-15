b3eef3309ceaaa1091a0194429c700c2595f8199
bartkoelewijnnl <bart_koelewijn@hotmail.nl>
2026-09-15 07:33:38 +0000

    Deploying to gh-pages from main @ 02303d51e342ceb190a4e0227ce15e7dd089a8b3 🚀

---
+++ _next/static/e2d2zYF6YHi82cTgs9Mtq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/384-b419cdcd49406d05.js","static/css/99d94351442154f0.css","static/chunks/pages/index-d792aaed9cf76ee3.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
